67437866325c2b838926b048b6f3398ad35a8218
erenozger <erennozger@gmail.com>
2021-05-04 13:31:57 +0300

    reader added.

---
+++ src/Airport.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Airport {
+    private String alias;
+    private String city;
+
+    static ArrayList<Airport> airports = new ArrayList<Airport>();
+
+    public Airport() {
+    }
+
+    public Airport(String alias, String city) {
+        this.alias = alias;
+        this.city = city;
+        Airport.airports.add(this);
+    }
+
+    public static void setAllAirports(ArrayList<String> airportList) {
+        for (int i = 0; i < airportList.size(); i++) {
+            String[] splitLine = airportList.get(i).split("\t");
+            int aliasSize = splitLine.length;
+            String cityName = splitLine[0];
+            for (int j = 1; j < aliasSize; j++) {
+                new Airport(splitLine[j], cityName);
+            }
+        }
+    }
+
+    public static void printAllAirports() {
+        for (int i = 0; i < airports.size(); i++) {
+            System.out.println(airports.get(i));
+        }
+    }
+
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{alias= " + this.getAlias() + ", city= " + this.getCity() + "}";
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("initial");
+    public static String airportListFile;
+    public static String flightListFile;
+    public static String commandListFile;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        airportListFile = args[0];      //contains airport list
+        flightListFile = args[1];       //contains flight list
+        commandListFile = args[2];      //contains commands query
+
+        PrintStream o = new PrintStream(new File("output.txt"));
+        System.setOut(o);               // Prints to output file
+
+        Airport.setAllAirports(Reader.readFile(airportListFile));
+        Airport.printAllAirports();
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
